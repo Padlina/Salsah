f022e4dbac9b76d5ad02b89de940750f12e302a4
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2017-05-12 17:14:48 +0200

    Removes some minor css issues.

--- src/app/view/modules/kitchensink/kitchensink.component.ts
+++ src/app/view/modules/kitchensink/kitchensink.component.ts
@@ -2,17 +2,13 @@
  * Created by Sebastian Schüpbach (sebastian.schuepbach@unibas.ch) on 5/11/17.
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'salsah-nie-kitchensink',
     templateUrl: './kitchensink.component.html',
     styleUrls: [ './kitchensink.component.css' ]
 })
-export class KitchensinkComponent /*implements OnInit */ {
-    //ngOnInit(): void {
-        // throw new Error('Method not implemented.');
-    //}
-
+export class KitchensinkComponent {
 
 }
